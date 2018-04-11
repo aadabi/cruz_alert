5c62ba20b9021cd2af61e7748d283af8e08b78f4
Nicholas Mandel <nwmandel@gmail.com>
2018-04-11 11:52:43 -0700

    added environment setup for firebase

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
 import { AppRegistry, SectionList, StyleSheet, Text, View } from 'react-native';
+import firebase from 'react-native-firebase';
+import { GoogleSignin } from 'react-native-google-signin';
+
+
+import {
+   FIREBASE_API_KEY,
+   AUTH_DOMAIN,
+   DATABASE_URL,
+   FIREBASE_PROJECT_ID,
+   FIREBASE_STORAGE_BUCKET,
+   MESSAGE_ID
+} from 'react-native-dotenv';
+
 
 export default class SectionListBasics extends Component {
   render() {
