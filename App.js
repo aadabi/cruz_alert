429e09204e21729824d43d2212e0528afdef1416
harlandduman <dumanjacob@gmail.com>
2018-04-11 10:08:06 -0700

    Added basic hello world app

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { AppRegistry, SectionList, StyleSheet, Text, View } from 'react-native';
+
+export default class SectionListBasics extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22,
+   alignItems: 'center'
+  }
+});
