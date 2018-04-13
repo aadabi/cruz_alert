ed2f0175cdc521cce216e2882c7c317d7679f97b
Harland Duman <dumanjacob@gmail.com>
2018-04-12 22:59:02 -0700

    Added a basic text input

--- App.js
+++ App.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
-import { AppRegistry, SectionList, StyleSheet, Text, View } from 'react-native';
-import firebase from 'react-native-firebase';
-import { GoogleSignin } from 'react-native-google-signin';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+// import firebase from 'react-native-firebase';
+// import { GoogleSignin } from 'react-native-google-signin';
 
 
-import {
-   FIREBASE_API_KEY,
-   AUTH_DOMAIN,
-   DATABASE_URL,
-   FIREBASE_PROJECT_ID,
-   FIREBASE_STORAGE_BUCKET,
-   MESSAGE_ID
-} from 'react-native-dotenv';
+// import {
+//    FIREBASE_API_KEY,
+//    AUTH_DOMAIN,
+//    DATABASE_URL,
+//    FIREBASE_PROJECT_ID,
+//    FIREBASE_STORAGE_BUCKET,
+//    MESSAGE_ID
+// } from 'react-native-dotenv';
 
 
 export default class SectionListBasics extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {description: ''};
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World!</Text>
+        <TextInput
+          multiline
+          style={styles.descriptionInput}
+          placeholder="Enter a description for your alert here..."
+          onChangeText={(description) => this.setState({description})}
+        />
       </View>
     );
   }
@@ -27,7 +37,15 @@ export default class SectionListBasics extends Component {
 const styles = StyleSheet.create({
   container: {
    flex: 1,
-   paddingTop: 22,
-   alignItems: 'center'
+   padding: 20,
+   paddingTop: 50
+  },
+  descriptionInput: {
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    height: '40%',
+    padding: 10,
+    paddingTop: 10
   }
 });
