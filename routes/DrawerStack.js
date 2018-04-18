4e94347655c4277a00d055399c8c2324e0c7e046
Harland Duman <dumanjacob@gmail.com>
2018-04-18 15:08:24 -0700

    Added a menu button on the header

--- routes/DrawerStack.js
+++ routes/DrawerStack.js
@@ -1,10 +1,22 @@
+import React from "react";
+import { Text } from "react-native";
 import { DrawerNavigator } from "react-navigation";
 import LogoutScreen from "../components/LogoutScreen";
 import SubmitReportScreen from "../components/SubmitReportScreen";
 
-const DrawerStack = DrawerNavigator({
-  LogoutScreen: { screen: LogoutScreen },
-  SubmitReportScreen: { screen: SubmitReportScreen }
-});
+const DrawerStack = DrawerNavigator(
+  {
+    LogoutScreen: { screen: LogoutScreen },
+    SubmitReportScreen: { screen: SubmitReportScreen }
+  },
+  {
+    headerMode: "float",
+    navigationOptions: ({ navigation }) => ({
+      headerLeft: (
+        <Text onPress={() => navigation.navigate("DrawerOpen")}>Menu</Text>
+      )
+    })
+  }
+);
 
 export default DrawerStack;
