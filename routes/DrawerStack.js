41101e5058aa0e85ca1f6d15f5dc9d70834d55bf
Harland Duman <dumanjacob@gmail.com>
2018-04-18 11:54:04 -0700

    Added submit report screen back into the drawer

--- routes/DrawerStack.js
+++ routes/DrawerStack.js
@@ -1,8 +1,10 @@
 import { DrawerNavigator } from 'react-navigation';
 import LogoutScreen from '../components/LogoutScreen';
+import SubmitReportScreen from '../components/SubmitReportScreen';
 
 const DrawerStack = DrawerNavigator({
-  LogoutScreen: { screen: LogoutScreen }
+  LogoutScreen: { screen: LogoutScreen },
+  SubmitReportScreen: { screen: SubmitReportScreen}
 });
 
 export default DrawerStack
