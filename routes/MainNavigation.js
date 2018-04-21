f6a4b35d1ed4b5a3fc88886c4e04a09f2ac1be5f
Harland Duman <dumanjacob@gmail.com>
2018-04-20 18:54:34 -0700

    Added a blank user reports screen

--- routes/MainNavigation.js
+++ routes/MainNavigation.js
@@ -5,10 +5,12 @@ import { DrawerNavigator, StackNavigator } from "react-navigation";
 import SubmitReportScreen from "../components/SubmitReportScreen";
 import DrawerContent from "../components/DrawerContent";
 import PublicReportsFeed from "../components/PublicReportsFeed";
+import UserReports from "../components/UserReports";
 
 const MainDrawerNavigation = DrawerNavigator(
   {
-    Home: { screen: PublicReportsFeed }
+    Home: { screen: PublicReportsFeed },
+    UserReports: { screen: UserReports }
   },
   {
     contentComponent: DrawerContent
