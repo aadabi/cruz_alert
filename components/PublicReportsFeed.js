a40500fa2ca9a6371254e5f7a6eb957779362597
JonnyMoli95 <molinacjony@gmail.com>
2018-04-23 21:49:59 -0700

    merge conflicts

---
+++ components/PublicReportsFeed.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { StackNavigator } from "react-navigation";
+import { Text } from "react-native";
+
+class PublicReportsFeed extends Component {
+  render() {
+    return (
+      <Text style={{ textAlign: "center", padding: 25 }}>
+        Public reports feed will go here!
+      </Text>
+    );
+  }
+}
+
+const PublicReportsFeedStackNavigator = StackNavigator({
+  PublicReportsFeed: {
+    screen: PublicReportsFeed,
+    navigationOptions: ({ navigation }) => ({
+      headerLeft: (
+        <Text
+          onPress={() => navigation.navigate("DrawerOpen")}
+          style={{ paddingLeft: 20 }}
+        >
+          Menu
+        </Text>
+      ),
+      headerRight: (
+        <Text
+          onPress={() => navigation.navigate("SubmitReportModal")}
+          style={{ paddingRight: 20 }}
+        >
+          New Report
+        </Text>
+      )
+    })
+  }
+});
+
+export default PublicReportsFeedStackNavigator;
