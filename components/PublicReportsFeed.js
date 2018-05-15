7eb271004b18f953b10f40e7407541d28bf2e651
harlandduman <dumanjacob@gmail.com>
2018-05-14 19:56:19 -0700

    Basic FlatList displays the text of each report

--- components/PublicReportsFeed.js
+++ components/PublicReportsFeed.js
@@ -1,170 +1,127 @@
 import React, { Component } from "react";
 import { StackNavigator, TabNavigator } from "react-navigation";
-import { Alert, Text, View, StyleSheet, Image, TouchableHighlight, ListView } from "react-native";
+import {
+  Alert,
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableHighlight,
+  FlatList
+} from "react-native";
 import firebase from "react-native-firebase";
 import CampusMap from "./CampusMap";
 
-
 class PublicReportsFeed extends React.Component {
-  static navigationOptions = {
-    drawerLabel: 'Home reports',
-    drawerIcon: () => (
-      <Image
-        source= {require('../components/images/home.png')}
-        style={{width: 40, height: 40, resizeMode: 'stretch'}}
-      />
-    )
-  }
-
   constructor(props) {
     super(props);
-    this.itemsRef = firebase.database().ref('/reports/public');
-    this.state = ({
-      dataSource: new ListView.DataSource({ rowHasChanged: (row1, row2) => row1 !== row2})
-    });
-    this.items = [];
+    this.state = {
+      reports: []
+    };
   }
 
-  listenForItems(itemsRef) {
-    itemsRef.limitToLast(15).on('value', (snap) => {
-      const items = [];
-      snap.forEach((child) => {
-        items.push({
-          uid: child.val().uid,
+  static navigationOptions = {
+    drawerLabel: "Home reports",
+    drawerIcon: () => (
+      <Image
+        source={require("../components/images/home.png")}
+        style={{ width: 40, height: 40, resizeMode: "stretch" }}
+      />
+    )
+  };
+
+  fetchReports() {
+    const reportsRef = firebase.database().ref("/reports/public");
+    reportsRef.once("value", snap => {
+      const reports = [];
+      snap.forEach(child => {
+        reports.push({
           category: child.val().category,
           description: child.val().description
         });
       });
-      const result = items.reverse()
-      this.setState({
-        dataSource: this.state.dataSource.cloneWithRows(result)
-      });
+      reports.reverse();
+      this.setState({ reports });
     });
   }
 
   componentDidMount() {
-    this.listenForItems(this.itemsRef);
+    this.fetchReports();
   }
-
-  renderRow(rowData) {
-    return (
-      <TouchableHighlight
-        underlayColor='#FFF7D6'
-        onPress={()=>this.props.navigation.navigate('Detail',
-          {description: rowData.description, category: rowData.category})}>
-        <View>
-          <View style={styles.row}>
-            <Text style={styles.descriptionTitle}>{rowData.category}</Text>
-            <Text style={styles.descriptionText}>{rowData.description}</Text>
-          </View>
-          <View style={styles.separator} />
-        </View>
-      </TouchableHighlight>
-    );
-  }
-
 
   render() {
     return (
-      <View style={styles.appContainer}>
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderRow.bind(this)}
-        enableEmptySection={true}
-        style={{flex:1}} />
-    </View>
+      <View>
+        <FlatList
+          data={this.state.reports}
+          renderItem={({ item }) => <Text>{item.description}</Text>}
+        />
+      </View>
     );
   }
 }
 
-const Detail = (props) => {
-    return(
-        <View>
-          <Text style={styles.detailsTitle}> {props.navigation.state.params.category} </Text>
-          <Text style={styles.detailsText}> {props.navigation.state.params.description} </Text>
-        </View>
-    );
-}
+const Detail = props => {
+  return (
+    <View>
+      <Text style={styles.detailsTitle}>
+        {" "}
+        {props.navigation.state.params.category}{" "}
+      </Text>
+      <Text style={styles.detailsText}>
+        {" "}
+        {props.navigation.state.params.description}{" "}
+      </Text>
+    </View>
+  );
+};
 
-const PublicTabNavigation = TabNavigator({
-  Feed: { screen: PublicReportsFeed },
-  CampusMap: { screen: CampusMap },
-}, {
-  tabBarPosition: 'bottom',
-  tabBarOptions: {
-    activeTintColor: '#FFFF52',
-    swipeEnabled: false,
-    activeBackgroundColor: '#0067A6',
-    inactiveTintColor: '#D1D1D1',
-    labelStyle: {
-      fontSize: 15,
-      padding: 1
+const PublicTabNavigation = TabNavigator(
+  {
+    Feed: { screen: PublicReportsFeed },
+    CampusMap: { screen: CampusMap }
+  },
+  {
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      activeTintColor: "#FFFF52",
+      swipeEnabled: false,
+      activeBackgroundColor: "#0067A6",
+      inactiveTintColor: "#D1D1D1",
+      labelStyle: {
+        fontSize: 15,
+        padding: 1
+      }
     }
   }
-});
+);
 
 const PublicReportsFeedStackNavigator = StackNavigator({
   PublicReportsFeed: {
     screen: PublicTabNavigation,
     navigationOptions: ({ navigation }) => ({
-      backgroundColor: '#FFFF52',
+      backgroundColor: "#FFFF52",
       headerLeft: (
         <Text
           onPress={() => navigation.navigate("DrawerOpen")}
-          style={{ paddingLeft: 20 }}>
+          style={{ paddingLeft: 20 }}
+        >
           Menu
         </Text>
       ),
       headerRight: (
         <Text
           onPress={() => navigation.navigate("SubmitReportModal")}
-          style={{ paddingRight: 20 }}>
+          style={{ paddingRight: 20 }}
+        >
           New Report
         </Text>
       )
     })
   },
-  Detail: {screen: Detail}
+  Detail: { screen: Detail }
 });
 
-var styles = StyleSheet.create({
-  appContainer:{
-    backgroundColor:'#D1D1D1',
-    flex: 1
-  },
-  row: {
-    flexDirection: 'row',
-    padding: 12,
-    height: 55
-  },
-  separator: {
-    height: 0.5,
-    backgroundColor: '#FFFF52',
-  },
-  descriptionText: {
-    fontSize: 21,
-    fontFamily: "normal",
-    color: '#494949',
-    flex: 2
-  },
-  descriptionTitle: {
-    fontSize: 23,
-    fontFamily: "monospace",
-    color: '#494949',
-    flex: 1,
-    fontWeight: '500',
-  },
-  detailsText: {
-    fontFamily: "serif",
-    fontSize: 30,
-    color: '#0067a6'
-  },
-  detailsTitle: {
-    fontSize: 26,
-    fontFamily: "monospace",
-    fontWeight: 'bold',
-    color: '#0067a6'
-  }
-});
+var styles = StyleSheet.create({});
 
 export default PublicReportsFeedStackNavigator;
