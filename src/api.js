7e142b8ab4d7002d4e9aee8ecf8aa2a5b6860772
Nicholas Mandel <nwmandel@gmail.com>
2018-05-19 17:03:27 -0700

    initial thanking api, still need to reload ui and style button

--- src/api.js
+++ src/api.js
@@ -20,13 +20,18 @@ const getReports = ref => {
     ref.once("value", snap => {
       const reports = [];
       snap.forEach(child => {
-        const { category, description, thankCount, usersThanked } = child.val();
-        const hasUserThanked = false;
+        const { title, category, description, thankCount, usersThanked } = child.val();
+        const temp = child.val().hasUserThanked;
+        // encode as string to pass into array
+        const hasUserThanked = JSON.stringify(temp);
+        const currKey = child.key;
         reports.push({
+          title,
+          currKey,
           category,
           description,
           thankCount,
-          hasUserThanked
+          hasUserThanked,
         });
       });
       resolve(reports.reverse());
@@ -57,6 +62,53 @@ export const submitReport = reportData => {
     .set(report);
 };
 
-const toggleThank = reportID => {
-  // TODO: get the user and toggle thank for that user
+export const toggleThank = (reportID, prevThankCount, prevHasUserThanked) => {
+  const { uid } = firebase.auth().currentUser;
+  // check if user already thanked report
+  firebase
+    .database()
+    .ref(`/reports/public/${reportID}/hasUserThanked`)
+    .once("value", snap => {
+      const findUser = snap.val();
+      if (findUser == null) {
+        incrementThank(uid, reportID, 0, {});
+      }
+      console.log("findUser: ", findUser[uid]);
+      // check if user has thanked this report
+      if (findUser[uid] == true) {
+        //decrement
+        console.log("decrement");
+        decrementThank(uid, reportID, prevThankCount, prevHasUserThanked);
+      } else {
+        //increment
+        console.log("increment");
+        incrementThank(uid, reportID, prevThankCount, prevHasUserThanked);
+      }
+  });
+  
+  
 };
+
+const incrementThank = (uid, reportID, prevThankCount, prevHasUserThanked) => {
+    const thankCount = prevThankCount + 1;
+    const hasUserThanked = prevHasUserThanked;
+    hasUserThanked[uid] = true;
+    const thankData = { thankCount, hasUserThanked };
+    firebase
+      .database()
+      .ref(`/reports/public/${reportID}/`)
+      .update(thankData);
+}
+
+const decrementThank = (uid, reportID, prevThankCount, _prevHasUserThanked) => {
+  // decode json object
+  const prevHasUserThanked = JSON.parse(_prevHasUserThanked);
+  const thankCount = prevThankCount - 1;
+  delete prevHasUserThanked[uid];
+  const hasUserThanked = prevHasUserThanked;
+  const thankData = { thankCount, hasUserThanked }
+  firebase
+    .database()
+    .ref(`/reports/public/${reportID}/`)
+    .update(thankData);
+}
--- src/components/Report.js
+++ src/components/Report.js
@@ -1,15 +1,35 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { toggleThank, getPublicReports } from "../api";
 
 const Report = props => {
   console.log(props);
+  //TODO: change the styling on the thank "button"
+  const currKey = props.report.currKey;
+  const currThankCount = props.report.thankCount;
+  const currHasUserThanked = props.report.hasUserThanked;
+  //TODO: still needs to reload the UI of updated thank count
   return (
     <View style={styles.container}>
+      <Text>{props.report.title}</Text>
       <Text>{props.report.category}</Text>
       <Text>{props.report.description}</Text>
+      <Text>{props.report.thankCount}</Text>
+      <TouchableOpacity 
+        onPress={thank.bind(this, currKey, currThankCount, currHasUserThanked)}
+        color="#841584"
+        title="Thanks"
+        style={styles.button}>
+        <Text style={styles.buttonText}> Thanks! </Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const thank = (key, prevThankCount, prevHasUserThanked) => {
+  toggleThank(key, prevThankCount, prevHasUserThanked);
+  getPublicReports();
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -17,7 +37,21 @@ const styles = StyleSheet.create({
     marginBottom: 10
   },
   category: {},
-  description: {}
+  description: {},
+  button: {
+    flex: 2,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    backgroundColor: 'powderblue',
+  },
+  buttonText: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontWeight: 'bold',
+  }
 });
 
 export default Report;
